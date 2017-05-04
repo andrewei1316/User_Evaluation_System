1256a534896c09503c861d30b22a0a94d11b364c
Andrewei <hsw19930501@gmail.com>
2017-05-04 23:53:20 +0800

    重写gitignore, 更改uwsgi

--- ues/ues/settings.py
+++ ues/ues/settings.py
@@ -143,7 +143,7 @@ CACHES = {
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/1.8/howto/static-files/
 
-STATIC_URL = '/static/'
+STATIC_URL = '/ues/static/'
 STATICFILES_DIRS = (
     os.path.join(BASE_DIR, 'static'),
 )
